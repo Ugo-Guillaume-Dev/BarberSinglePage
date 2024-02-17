0ede0e66010fb2222ea48c14fd57e72fd918d513
GuillaumeLeger31 <guillaumeleger140@mail.com>
2024-02-17 14:58:54 +0100

    update carrousel

--- js/script.js
+++ js/script.js
@@ -9,3 +9,25 @@ function initMap() {
     map: map
   })
 }
+
+// let slideIndex = 0;
+// showSlides();
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+const folder  = ''
+async function showSlides() {
+  let i;
+  const slides = document.getElementsByClassName("test");
+  for (i = 0; i < slides.length; i++) {
+    const regex = /coupe_(\d+)\.png/
+    const match =  slides[i].src.match(regex)
+    let valeur = 1
+    if (parseInt(match[1]) < 8) valeur = parseInt(match[1]) + 1
+    const newUrl = `http://127.0.0.1:5500/img/coupe/coupe_${valeur}.png`
+    slides[i].src = newUrl
+  }
+  // setTimeout(showSlides, 2000);
+}
